f1b325dfa6beebe45468d23fc4c64a708f3d40f7
B-Otermans <114000449+B-Otermans@users.noreply.github.com>
2025-02-27 17:19:57 +0100

    added documentation

--- ImageRegistration/augmentation.py
+++ ImageRegistration/augmentation.py
@@ -3,6 +3,16 @@ import torchio as tio
 
 
 def save_augmentation(aug_img: object, orig_path: str, extension: str) -> str:
+    '''Saves (augmented) image by inserting extension into original path.
+    
+    parameters
+    aug_img: image object
+    orig_path: file path of unaugmented image
+    extension: text to be added after original filepath when saving new image
+    
+    returns
+    new_path: file path of the saved augmented image
+    '''
     dot_idx = orig_path.rfind(".")
     new_path = orig_path[:dot_idx] + extension + orig_path[dot_idx:]
     sitk.WriteImage(aug_img, new_path)
@@ -11,6 +21,16 @@ def save_augmentation(aug_img: object, orig_path: str, extension: str) -> str:
 
 def reldef(img_path: str, deform_params: dict = {"num_control_points": (7, 7, 7), "locked_borders": 2},
            ext: str = "_aug_reldef") -> str:
+    '''Random elastic deformation of image located at input file path.
+
+    parameters
+    img_path: file path to image
+    deform_params: dictionary of parameters for 'tio.RandomElasticDeformation()'
+    ext: text to be added after original filepath when saving new image
+    
+    returns
+    aug_path: file path of the saved augmented image
+    '''
     img = sitk.ReadImage(img_path)
     deform = tio.RandomElasticDeformation(**deform_params)
     aug_img = deform(img)
@@ -19,6 +39,15 @@ def reldef(img_path: str, deform_params: dict = {"num_control_points": (7, 7, 7)
 
 
 def normalise(img_path: str, ext: str = "_aug_norm") -> str:
+    '''Normalise (mean=0, var=1) image located at input file path.
+
+    parameters
+    img_path: file path to image
+    ext: text to be added after original filepath when saving new image
+    
+    returns
+    aug_path: file path of the saved augmented image
+    '''
     img = sitk.ReadImage(img_path)
     norm_filter = sitk.NormalizeImageFilter()
     aug_img = norm_filter.Execute(img)
@@ -27,6 +56,16 @@ def normalise(img_path: str, ext: str = "_aug_norm") -> str:
 
 
 def rescale(img_path: str, bounds: tuple[int], ext: str = "_aug_rescale") -> str:
+    '''Rescale pixel values of image located at input file path.
+
+    parameters
+    img_path: file path to image
+    bounds: tuple of new min and max pixel intensity
+    ext: text to be added after original filepath when saving new image
+    
+    returns
+    aug_path: file path of the saved augmented image
+    '''
     img = sitk.ReadImage(img_path)
     aug_img = sitk.RescaleIntensity(img,bounds[0], bounds[1])
     aug_path = save_augmentation(aug_img, img_path, extension=ext)
@@ -34,6 +73,16 @@ def rescale(img_path: str, bounds: tuple[int], ext: str = "_aug_rescale") -> str
 
 
 def random_gamma(img_path: str, bounds: tuple[float], ext: str = "_aug_rgam") -> str:
+    '''Appply random gamma correction to image located at input file path.
+
+    parameters
+    img_path: file path to image
+    bounds: tuple of min and max gamma
+    ext: text to be added after original filepath when saving new image
+    
+    returns
+    aug_path: file path of the saved augmented image
+    '''
     img = sitk.ReadImage(img_path)
     rgam_transform = tio.transforms.RandomGamma(log_gamma=bounds)
     aug_img = rgam_transform(img)
@@ -41,9 +90,19 @@ def random_gamma(img_path: str, bounds: tuple[float], ext: str = "_aug_rgam") ->
     return aug_path
 
 
-def random_rotation(img_path: str, degree_bounds: tuple[int], ext: str = "_aug_rot") -> str:
+def random_rotation(img_path: str, bounds: tuple[int], ext: str = "_aug_rot") -> str:
+    '''Appply random rotation to image located at input file path.
+
+    parameters
+    img_path: file path to image
+    bounds: tuple of min and max rotation in every axis
+    ext: text to be added after original filepath when saving new image
+    
+    returns
+    aug_path: file path of the saved augmented image
+    '''
     img = sitk.ReadImage(img_path)
-    rotation = tio.RandomAffine(degrees=degree_bounds, scales=0)
+    rotation = tio.RandomAffine(degrees=bounds, scales=0)
     aug_img = rotation(img)
     aug_path = save_augmentation(aug_img, img_path, extension=ext)
     return aug_path
