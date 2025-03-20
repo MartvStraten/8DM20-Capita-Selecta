32b6d9443df9da3ab15ca774f1d85389f2b1eb02
MartvStraten <131674849+MartvStraten@users.noreply.github.com>
2025-03-20 14:15:47 +0100

    Added U-NET & VAE

--- MachineLearning/code/u_net.py
+++ MachineLearning/code/u_net.py
@@ -1,162 +1,104 @@
 import torch
 import torch.nn as nn
 
-
 class Block(nn.Module):
-    """A representation for the basic convolutional building block of the unet
-
-    Parameters
-    ----------
-    in_ch : int 
-        number of input channels to the block
-    out_ch : int 
-        number of output channels of the block
+    """Class for the basic convolutional building block of the unet
     """
-
     def __init__(self, in_ch, out_ch):
+        """Constructor.
+        :param in_ch: number of input channels to the block
+        :param out_ch: number of output channels of the block
+        """
         super().__init__()
         self.conv1 = nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(out_ch)
-        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
-        self.bn2 = nn.BatchNorm2d(out_ch)
         self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
 
     def forward(self, x):
-        """Performs the forward pass of the block.
-
-        Parameters
-        ----------
-        x : torch.Tensor
-            the input to the block
-
-        Returns
-        -------
-        x : torch.Tensor
-            the output of the forward pass
+        """Returns the output of a forward pass of the block
+        :param x: the input tensor
+        :return: the output tensor of the block
         """
         # a block consists of two convolutional layers
         # with ReLU activations
-
         x = self.conv1(x)
-        x = self.bn1(x)
         x = self.relu(x)
         x = self.conv2(x)
-        x = self.bn2(x)
         x = self.relu(x)
         return x
 
-
 class Encoder(nn.Module):
-    """A representation for the encoder part of the unet.
-
-    Parameters
-    ----------
-    chs : tuple
-        holds the number of input channels of each block in the encoder
-
+    """Class for the encoder part of the unet.
     """
-
     def __init__(self, chs=(1, 64, 128, 256, 512, 1024)):
+        """Constructor.
+        :param chs: tuple giving the number of input channels of each block in the encoder
+        """
         super().__init__()
-        # convolutional blocks
         self.enc_blocks = nn.ModuleList(
             [Block(chs[i], chs[i + 1]) for i in range(len(chs) - 1)]
         )
-        # max pooling
         self.pool = nn.MaxPool2d(2)
 
     def forward(self, x):
-        """Performs the forward pass for all blocks in the encoder.
-
-        Parameters
-        ----------
-        x : torch.Tensor
-            input 
-
-        Returns
-        -------
-        list[torch.Tensor]
-            contains the outputs of each block in the encoder
+        """Returns the list of the outputs of all the blocks in the encoder
+        :param x: input image tensor
         """
         ftrs = []  # a list to store features
         for block in self.enc_blocks:
-            x = block(x)        
-            # # save features to concatenate to decoder blocks
+            x = block(x)
+            # save features to concatenate to decoder blocks
             ftrs.append(x)
             x = self.pool(x)
-        ftrs.append(x) # save features
+        ftrs.append(x)
         return ftrs
 
-
 class Decoder(nn.Module):
-    """A representation for the decoder part of the unet.
-
-    Layers consist of transposed convolutions followed by convolutional blocks.
-
-    Parameters
-    ----------
-    chs : tuple
-        holds the number of input channels of each block in the decoder
+    """Class for the decoder part of the unet.
     """
 
     def __init__(self, chs=(1024, 512, 256, 128, 64)):
+        """Constructor.
+        :param chs: tuple giving the number of input channels of each block in the decoder
+        """
         super().__init__()
         self.chs = chs
         self.upconvs = nn.ModuleList(
-            # TODO: transposed convolution
+            [nn.ConvTranspose2d(chs[i], chs[i], 2, 2) for i in range(len(chs) - 1)]
         )
         self.dec_blocks = nn.ModuleList(
-            # TODO: convolutional blocks
+            [Block(2 * chs[i], chs[i + 1]) for i in range(len(chs) - 1)]
         )
 
     def forward(self, x, encoder_features):
-        """Performs the forward pass for all blocks in the decoder. 
-        
-        Parameters
-        ----------
-        x : torch.Tensor
-            input to the decoder
-        encoder_features: list
-            contains the encoder features to be concatenated to the corresponding level of the decoder
-
-        Returns
-        -------
-        x : torch.Tensor
-            output of the decoder 
+        """Returns the output of the decoder part of the unet
+        :param x: input tensor to the decoder
+        :param encoder_features: list of the encoder features to be concatenated to the corresponding level of the decoder
         """
         for i in range(len(self.chs) - 1):
-            # transposed convolution
-            # TODO
+            x = self.upconvs[i](x)
             # get the features from the corresponding level of the encoder
-            # TODO
+            enc_ftrs = encoder_features[i]
             # concatenate these features to x
-            x = # TODO
+            x = torch.cat([x, enc_ftrs], dim=1)
             # convolutional block
-            x = # TODO
-
-
+            x = self.dec_blocks[i](x)
         return x
 
-
 class UNet(nn.Module):
-    """A representation for a unet
-    
-    Parameters
-    ----------
-    enc_chs : tuple
-        holds the number of input channels of each block in the encoder
-    dec_chs : tuple
-        holds the number of input channels of each block in the decoder
-    num_classes : int
-        number of output classes of the segmentation
+    """Class for the unet
     """
-
     def __init__(
         self,
         enc_chs=(1, 64, 128, 256),
         dec_chs=(256, 128, 64, 32),
         num_classes=1,
     ):
+        """Constructor.
+        :param enc_chs: tuple giving the number of input channels of each block in the encoder
+        :param dec_chs: tuple giving the number of input channels of each block in the encoder
+        :param num_classes: number of output classes of the segmentation
+        """
         super().__init__()
         self.encoder = Encoder(enc_chs)
         self.decoder = Decoder(dec_chs)
@@ -165,22 +107,10 @@ class UNet(nn.Module):
         )  # output layer
 
     def forward(self, x):
-        """Performs the forward pass of the unet.
-       
-        Parameters
-        ----------
-        x : torch.Tensor
-            the input to the unet (image)
-
-        Returns
-        -------
-        out : torch.Tensor
-            unet output, the logits of the predicted segmentation mask
+        """Returns the output of a forward pass of the unet
+        :param x: the input tensor to the unet
         """
-
-        # TODO
-        
         enc_ftrs = self.encoder(x)
-        # then apply decoding 
-        # and output layer
+        out = self.decoder(enc_ftrs[::-1][0], enc_ftrs[::-1][1:])
+        out = self.head(out)
         return out
--- MachineLearning/code/vae.py
+++ MachineLearning/code/vae.py
@@ -3,10 +3,8 @@ import torch.nn as nn
 
 l1_loss = torch.nn.L1Loss()
 
-
 class Block(nn.Module):
     """Basic convolutional building block
-
     Parameters
     ----------
     in_ch : int
@@ -14,35 +12,34 @@ class Block(nn.Module):
     out_ch : int     
         number of output channels of the block
     """
-
     def __init__(self, in_ch, out_ch):
         super().__init__()
         self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
-        self.bn1 = # TODO   # batch normalisation
+        self.bn1 = nn.BatchNorm2d(out_ch)
         self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
-        self.bn2 = # TODO 
-        self.lrelu =  # TODO  # leaky ReLU
-
+        self.bn2 = nn.BatchNorm2d(out_ch)
+        self.lrelu =  nn.LeakyReLU(0.1)
 
     def forward(self, x):
         """Performs a forward pass of the block
-       
         x : torch.Tensor
             the input to the block
         torch.Tensor
             the output of the forward pass
         """
         # a block consists of two convolutional layers
-        # with ReLU activations
+        # with LeakyReLU activations
         # use batch normalisation
-
-        # TODO
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.lrelu(x)
         return x
 
 
 class Encoder(nn.Module):
     """The encoder part of the VAE.
-
     Parameters
     ----------
     spatial_size : list[int]
@@ -52,46 +49,42 @@ class Encoder(nn.Module):
     chs : tuple
         hold the number of input channels for each encoder block
     """
-
     def __init__(self, z_dim=256, chs=(1, 64, 128, 256), spatial_size=[64, 64]):
         super().__init__()
         # convolutional blocks
         self.enc_blocks = nn.ModuleList(
-            # TODO
+            [Block(chs[i], chs[i + 1]) for i in range(len(chs) - 1)]
         )
         # max pooling
-        self.pool = # TODO
+        self.pool = nn.MaxPool2d(2)
         # height and width of images at lowest resolution level
-        _h, _w = # TODO
-
+        _h, _w = (
+            spatial_size[0] // (2**(len(chs) - 1)), 
+            spatial_size[1] // (2**(len(chs) - 1))
+        )
         # flattening
         self.out = nn.Sequential(nn.Flatten(1), nn.Linear(chs[-1] * _h * _w, 2 * z_dim))
 
     def forward(self, x):
         """Performs the forward pass for all blocks in the encoder.
-
         Parameters
         ----------
         x : torch.Tensor
             input image to the encoder
-
         Returns
         -------
         list[torch.Tensor]    
             a tensor with the means and a tensor with the log variances of the
             latent distribution
         """
-
         for block in self.enc_blocks:
-            # TODO: conv block           
-            # TODO: pooling 
-        # TODO: output layer          
+            x = block(x)          
+            x = self.pool(x) 
+        x = self.out(x)          
         return torch.chunk(x, 2, dim=1)  # 2 chunks, 1 each for mu and logvar
-
 
 class Generator(nn.Module):
     """Generator of the VAE
-
     Parameters
     ----------
     z_dim : int 
@@ -103,28 +96,25 @@ class Generator(nn.Module):
     w : int, optional
         width of image at lowest resolution level, by default 8    
     """
-
     def __init__(self, z_dim=256, chs=(256, 128, 64, 32), h=8, w=8):
-
         super().__init__()
         self.chs = chs
         self.h = h  
         self.w = w  
-        self.z_dim = z_dim  
+        self.z_dim = z_dim 
+
         self.proj_z = nn.Linear(
             self.z_dim, self.chs[0] * self.h * self.w
         )  # fully connected layer on latent space
         self.reshape = lambda x: torch.reshape(
             x, (-1, self.chs[0], self.h, self.w)
         )  # reshaping
-
         self.upconvs = nn.ModuleList(
-            # TODO: transposed convolution            
-        )
-
+            [nn.ConvTranspose2d(chs[i], chs[i + 1], 2, 2) for i in range(len(chs) - 1)]            
+        ) # transposed convolution
         self.dec_blocks = nn.ModuleList(
-            # TODO: conv block           
-        )
+            [Block(chs[i + 1], chs[i + 1]) for i in range(len(chs) - 1)]           
+        ) # conv blocks
         self.head = nn.Sequential(
             nn.Conv2d(self.chs[-1], 1, 1),
             nn.Tanh(),
@@ -132,28 +122,24 @@ class Generator(nn.Module):
 
     def forward(self, z):
         """Performs the forward pass of generator
-
         Parameters
         ----------
         z : torch.Tensor
             input to the generator
-        
         Returns
         -------
         x : torch.Tensor
-        
         """
-        x = # TODO: fully connected layer
-        x = # TODO: reshape to image dimensions
+        x = self.proj_z(z)
+        x = self.reshape(x)
         for i in range(len(self.chs) - 1):
-            # TODO: transposed convolution
-            # TODO: convolutional block
+            x = self.upconvs[i](x)
+            x = self.dec_blocks[i](x)
         return self.head(x)
 
 
 class VAE(nn.Module):
     """A representation of the VAE
-
     Parameters
     ----------
     enc_chs : tuple 
@@ -161,27 +147,18 @@ class VAE(nn.Module):
     dec_chs : tuple 
         holds the number of input channels of each block in the decoder
     """
-    def __init__(
-        self,
-        z_dim=256,
-        enc_chs=(1, 64, 128, 256),
-        dec_chs=(256, 128, 64, 32),
-    ):
+    def __init__(self, z_dim=256, enc_chs=(1, 64, 128, 256), dec_chs=(256, 128, 64, 32)):
         super().__init__()
         self.encoder = Encoder(z_dim, enc_chs)
         self.generator = Generator(z_dim, dec_chs)
 
-
-
     def forward(self, x):
         """Performs a forwards pass of the VAE and returns the reconstruction
         and mean + logvar.
-
         Parameters
         ----------
         x : torch.Tensor
             the input to the encoder
-
         Returns
         -------
         torch.Tensor
@@ -193,18 +170,14 @@ class VAE(nn.Module):
         """
         mu, logvar = self.encoder(x)
         latent_z = sample_z(mu, logvar)
-        
         output = self.generator(latent_z)
-        
         return output, mu, logvar
 
 
 def get_noise(n_samples, z_dim, device="cpu"):
     """Creates noise vectors.
-    
     Given the dimensions (n_samples, z_dim), creates a tensor of that shape filled with 
     random numbers from the normal distribution.
-
     Parameters
     ----------
     n_samples : int
@@ -216,10 +189,8 @@ def get_noise(n_samples, z_dim, device="cpu"):
     """
     return torch.randn(n_samples, z_dim, device=device)
 
-
 def sample_z(mu, logvar):
     """Samples noise vector from a Gaussian distribution with reparameterization trick.
-
     Parameters
     ----------
     mu : float
@@ -230,29 +201,24 @@ def sample_z(mu, logvar):
     eps = torch.randn(mu.size(), device=mu.device).to(mu.dtype)
     return (logvar / 2).exp() * eps + mu
 
-
 def kld_loss(mu, logvar):
     """Computes the KLD loss given parameters of the predicted 
     latent distribution.
-
     Parameters
     ----------
     mu : float
         the mean of the distribution
     logvar : float
         the log of the variance of the distribution
-
     Returns
     -------
     float
         the kld loss
-
     """
     return -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
 
 def vae_loss(inputs, recons, mu, logvar, beta=1):
     """Computes the VAE loss, sum of reconstruction and KLD loss
-
     Parameters
     ----------
     inputs : torch.Tensor
@@ -263,7 +229,6 @@ def vae_loss(inputs, recons, mu, logvar, beta=1):
         the predicted mean of the latent distribution
     logvar : float
         the predicted log of the variance of the latent distribution
-
     Returns
     -------
     float
