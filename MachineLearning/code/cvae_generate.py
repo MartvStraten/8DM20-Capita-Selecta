9647e290d94f8648b30ff03ba8e13bae7ac422a4
B-Otermans <114000449+B-Otermans@users.noreply.github.com>
2025-03-29 11:13:51 +0100

    unet-cvae-data results & code

---
+++ MachineLearning/code/cvae_generate.py
@@ -0,0 +1,119 @@
+import numpy as np
+import random
+import torch
+import torchio as tio
+import SimpleITK as sitk
+
+from torch.utils.data import DataLoader
+from pathlib import Path
+
+import utils
+import cvae
+
+
+def bin_masks(dataloader, bin_edges):
+    # initialise 16 bins
+    binned_masks =[[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+    for _, total_segment in dataloader:
+        # Loop over individual segmentations
+        for segment in total_segment:
+            # Calculate number of pixels
+            segment_np = segment.squeeze().numpy()
+            num_pixels = np.sum(segment_np)
+            bin_id = min(np.digitize(num_pixels, bin_edges) - 1, 15)
+            binned_masks[bin_id].append(segment.to(torch.int32).unsqueeze(0))
+            
+    return binned_masks
+
+
+def floodfill_data(bins, bin_edges, binned_masks, cvae_model):
+    for folder_nr in range(1,16):
+        print(f"populating folder: cvae ({folder_nr})")
+        new_mr = []
+        new_masks = []
+        path = f"D:\TUe\8DM20\8DM20-Capita-Selecta\DevelopmentData\cvae ({folder_nr})"
+        for _ in range(86):
+            # find bin with least entries
+            lowest_bin = np.argmin(bins)
+            # pick random mask from lowest bin
+            mask = random.choice(binned_masks[lowest_bin])
+
+            # apply reldef untill mask is not too large
+            reldef = tio.RandomElasticDeformation(max_displacement=(0,15,15), locked_borders=2)
+            aug_mask = torch.ceil(reldef(mask))
+            num_pixels = int(np.sum(aug_mask.squeeze().numpy()))
+            while num_pixels > 1000:
+                reldef = tio.RandomElasticDeformation(max_displacement=(0,15,15), locked_borders=2)
+                aug_mask = torch.ceil(reldef(mask))
+                num_pixels = int(np.sum(aug_mask.squeeze().numpy()))
+            
+            # generate image with cvae
+            noise = torch.randn(1, Z_DIM)
+            generated_mr = cvae_model.generator(noise, aug_mask)
+            # add mask and generated image to arrays
+            new_mr.append(generated_mr)
+            new_masks.append(aug_mask)
+
+            bin_id = min(np.digitize(num_pixels, bin_edges) - 1, 15)
+            bins[bin_id] += 1
+
+        new_mr = torch.cat(new_mr, 0).detach().numpy()
+        new_masks = torch.cat(new_masks, 0).detach().numpy()
+        sitk.WriteImage(sitk.GetImageFromArray(new_mr), path+"\mr_bffe.mhd")
+        sitk.WriteImage(sitk.GetImageFromArray(new_masks), path+"\prostaat.mhd")
+
+
+if __name__ == "__main__":
+    # directorys with data and to store training checkpoints and logs
+    DATA_DIR = Path.cwd() / "DevelopmentData"
+    CHECKPOINTS_DIR = r"D:\TUe\8DM20\8DM20-Capita-Selecta\MachineLearning\code\cvae_model_weights\cvae_model_new_loss.pth"
+    print(DATA_DIR)
+    print(CHECKPOINTS_DIR)
+
+    # data settings 
+    IMAGE_SIZE = [64, 64]
+    BATCH_SIZE = 1290
+    Z_DIM = 256
+
+    # find patient folders in training directory
+    # excluding hidden folders (start with .)
+    patients = [
+        path
+        for path in DATA_DIR.glob("*")
+        if not any(part.startswith(".") or part.startswith("c") for part in path.parts)
+    ]
+    print(patients)
+    # load training data and create DataLoader with batching and shuffling
+    dataset = utils.ProstateMRDataset(patients, IMAGE_SIZE)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        drop_last=False,
+        pin_memory=True
+    )
+
+    # Obtain all images in a tensor
+    total_prostate, total_segment = next(iter(dataloader))
+
+    # Load the trained model weights
+    cvae_model = cvae.CVAE()
+    cvae_model.load_state_dict(torch.load(CHECKPOINTS_DIR))
+    cvae_model.eval();
+
+    pixel_count = []
+    # Loop over segmentation inside dataloader
+    for total_prostate, total_segment in dataloader:
+        # Loop over individual segmentations
+        for segment in total_segment:
+            # Calculate number of pixels
+            segment = segment.squeeze().numpy()
+            num_pixels = np.sum(segment)
+            pixel_count.append(int(num_pixels))
+    pc_array = np.array(pixel_count)
+
+    pc_bins, pc_bin_edges = np.histogram(pc_array, 16)
+    binned_masks = bin_masks(dataloader, pc_bin_edges)
+
+    floodfill_data(pc_bins, pc_bin_edges, binned_masks, cvae_model);
+    
--- MachineLearning/code/utils.py
+++ MachineLearning/code/utils.py
@@ -142,3 +142,203 @@ class DiceBCELoss(nn.Module):
         BCE = nn.functional.binary_cross_entropy(outputs, targets, reduction="mean")
 
         return BCE + dice_loss
+
+
+class DatasetCVAE(torch.utils.data.Dataset):   
+    def __init__(self, paths, valid=False):
+        self.mr_image_list = []
+        self.mask_list = []
+        # load images
+        for path in paths:
+            self.mr_image_list.append(
+                sitk.GetArrayFromImage(sitk.ReadImage(path / "mr_bffe.mhd"))[:,0,:,:].astype(
+                    np.int32
+                )
+            )
+            self.mask_list.append(
+                sitk.GetArrayFromImage(sitk.ReadImage(path / "prostaat.mhd"))[:,0,:,:].astype(
+                    np.int32
+                )
+            )
+
+        # number of patients and slices in the dataset
+        self.no_patients = len(self.mr_image_list)
+        self.no_slices = self.mr_image_list[0].shape[0]
+
+        if valid:
+            self.img_transform = transforms.Compose(
+                [
+                    transforms.ToPILImage(mode="I"),
+                    transforms.ToTensor(),
+                ]
+            )
+        else:
+            # transforms to resize images
+            self.img_transform = transforms.Compose(
+                [
+                    transforms.ToPILImage(mode="I"),
+                    transforms.ToTensor(),
+                    transforms.RandomAffine(
+                        degrees=(-20, 20), translate=(0.1, 0.1), scale=(0.9, 1.1)
+                    ),
+                ]
+            )
+        # standardise intensities based on mean and std deviation
+        self.train_data_mean = np.mean(self.mr_image_list)
+        self.train_data_std = np.std(self.mr_image_list)
+        self.norm_transform = transforms.Normalize(
+            self.train_data_mean, self.train_data_std
+        )
+
+    def __len__(self):
+        """Returns length of dataset"""
+        return self.no_patients * self.no_slices
+
+    def __getitem__(self, index):
+        """Returns the preprocessing MR image and corresponding segementation
+        for a given index.
+
+        Parameters
+        ----------
+        index : int
+            index of the image/segmentation in dataset
+        """
+        # compute which slice an index corresponds to
+        patient = index // self.no_slices
+        the_slice = index - (patient * self.no_slices)
+
+        seed = np.random.randint(2147483647)  # make a seed with numpy generator
+        random.seed(seed)
+        torch.manual_seed(seed)
+
+        x = self.norm_transform(
+            self.img_transform(self.mr_image_list[patient][the_slice, ...]).float()
+        )
+        random.seed(seed)
+        torch.manual_seed(seed)
+        y = self.img_transform(
+            (self.mask_list[patient][the_slice, ...] > 0).astype(np.int32)
+        )
+        return x, y
+
+
+class CombinedProstateMRDataset(torch.utils.data.Dataset):
+    """Dataset containing prostate MR images.
+
+    Parameters
+    ----------
+    paths : list[Path]
+        paths to the patient data
+    img_size : list[int]
+        size of images to be interpolated to
+    """
+
+    def __init__(self, paths, img_size, valid=False):
+        self.mr_image_list = []
+        self.mask_list = []
+        # load images
+        for path in paths:
+            mr_image = sitk.GetArrayFromImage(sitk.ReadImage(path / "mr_bffe.mhd")).astype(np.int32)
+            mask = sitk.GetArrayFromImage(sitk.ReadImage(path / "prostaat.mhd")).astype(np.int32)
+            if len(mr_image.shape) == 4:
+                mr_image = mr_image.squeeze(1)
+                mask = mask.squeeze(1)
+            self.mr_image_list.append(mr_image)
+            self.mask_list.append(mask)
+
+        # number of patients and slices in the dataset
+        self.no_patients = len(self.mr_image_list)
+        self.no_slices = self.mr_image_list[0].shape[0]
+
+        if valid:
+            self.img_transform = transforms.Compose(
+                [
+                    transforms.ToPILImage(mode="I"),
+                    transforms.CenterCrop(256),
+                    transforms.Resize(img_size),
+                    transforms.ToTensor(),
+                ]
+            )
+            self.img_transform_cvae = transforms.Compose(
+                [
+                    transforms.ToPILImage(mode="I"),
+                    transforms.ToTensor(),
+                ]
+            )
+        else:
+            self.img_transform = transforms.Compose(
+                [
+                    transforms.ToPILImage(mode="I"),
+                    transforms.CenterCrop(256),
+                    transforms.Resize(img_size),
+                    transforms.ToTensor(),
+                    transforms.RandomAffine(
+                        degrees=(-20, 20), translate=(0.1, 0.1), scale=(0.9, 1.1)
+                    ),
+                ]
+            )
+            self.img_transform_cvae = transforms.Compose(
+                [
+                    transforms.ToPILImage(mode="I"),
+                    transforms.ToTensor(),
+                    transforms.RandomAffine(
+                        degrees=(-20, 20), translate=(0.1, 0.1), scale=(0.9, 1.1)
+                    ),
+                ]
+            )
+        
+        total_sum = 0
+        total_sq_sum = 0
+        total_count = 0
+        for arr in self.mr_image_list:
+            total_sum += arr.sum()
+            total_sq_sum += (arr**2).sum()  # Sum of squares
+            total_count += arr.size
+
+        mean_value = total_sum / total_count
+        std_value = np.sqrt((total_sq_sum / total_count) - (mean_value ** 2))
+        self.train_data_mean = mean_value
+        self.train_data_std = std_value
+
+        # standardise intensities based on mean and std deviation
+        # self.train_data_mean = np.mean(self.mr_image_list)
+        # self.train_data_std = np.std(self.mr_image_list)
+        self.norm_transform = transforms.Normalize(
+            self.train_data_mean, self.train_data_std
+        )
+
+    def __len__(self):
+        """Returns length of dataset"""
+        return self.no_patients * self.no_slices
+
+    def __getitem__(self, index):
+        """Returns the preprocessing MR image and corresponding segementation
+        for a given index.
+
+        Parameters
+        ----------
+        index : int
+            index of the image/segmentation in dataset
+        """
+        # compute which slice an index corresponds to
+        patient = index // self.no_slices
+        the_slice = index - (patient * self.no_slices)
+
+        seed = np.random.randint(2147483647)  # make a seed with numpy generator
+        random.seed(seed)
+        torch.manual_seed(seed)
+        x = self.mr_image_list[patient][the_slice, ...]
+        if x.shape[-1] == 64:
+            x = self.norm_transform(self.img_transform_cvae(self.mr_image_list[patient][the_slice, ...]).float())
+        else:
+            x = self.norm_transform(self.img_transform(self.mr_image_list[patient][the_slice, ...]).float())
+        
+        random.seed(seed)
+        torch.manual_seed(seed)
+        y = self.mask_list[patient][the_slice, ...] > 0
+        if y.shape[-1] == 64:
+            y = self.img_transform_cvae((y).astype(np.int32))
+        else:
+            y = self.img_transform((y).astype(np.int32))
+        
+        return x, y
