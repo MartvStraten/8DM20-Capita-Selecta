4eddde1ec992742c5fa3c2a6362f8a26ccfc2d62
B-Otermans <114000449+B-Otermans@users.noreply.github.com>
2025-03-31 13:20:42 +0200

    fix

--- MachineLearning/code/utils.py
+++ MachineLearning/code/utils.py
@@ -145,38 +145,70 @@ class DiceBCELoss(nn.Module):
 
 
 class DatasetCVAE(torch.utils.data.Dataset):   
-    def __init__(self, paths, valid=False):
+    def __init__(self, patient_paths, cvae_paths, img_size=0, valid=False):
         self.mr_image_list = []
         self.mask_list = []
+        self.pt_mr_image_list = []
+        self.pt_mask_list = []
+        self.cvae_mr_image_list = []
+        self.cvae_mask_list = []
         # load images
-        for path in paths:
-            self.mr_image_list.append(
-                sitk.GetArrayFromImage(sitk.ReadImage(path / "mr_bffe.mhd"))[:,0,:,:].astype(
+        for path in patient_paths:
+            self.pt_mr_image_list.append(
+                sitk.GetArrayFromImage(sitk.ReadImage(path / "mr_bffe.mhd")).astype(
                     np.int32
                 )
             )
-            self.mask_list.append(
-                sitk.GetArrayFromImage(sitk.ReadImage(path / "prostaat.mhd"))[:,0,:,:].astype(
+            self.pt_mask_list.append(
+                sitk.GetArrayFromImage(sitk.ReadImage(path / "prostaat.mhd")).astype(
                     np.int32
                 )
             )
 
-        # number of patients and slices in the dataset
-        self.no_patients = len(self.mr_image_list)
-        self.no_slices = self.mr_image_list[0].shape[0]
+        for path in cvae_paths:
+            self.cvae_mr_image_list.append(
+                sitk.GetArrayFromImage(sitk.ReadImage(path / "mr_bffe.mhd")).astype(
+                    np.float32
+                ).squeeze(1)
+            )
+            self.cvae_mask_list.append(
+                sitk.GetArrayFromImage(sitk.ReadImage(path / "prostaat.mhd")).astype(
+                    np.int32
+                ).squeeze(1)
+            )
+            
 
         if valid:
-            self.img_transform = transforms.Compose(
+            self.pt_img_transform = transforms.Compose(
                 [
                     transforms.ToPILImage(mode="I"),
+                    transforms.CenterCrop(256),
+                    transforms.Resize(img_size),
+                    transforms.ToTensor(),
+                ]
+            )
+
+            self.cvae_img_transform = transforms.Compose(
+                [
                     transforms.ToTensor(),
                 ]
             )
         else:
             # transforms to resize images
-            self.img_transform = transforms.Compose(
+            self.pt_img_transform = transforms.Compose(
                 [
                     transforms.ToPILImage(mode="I"),
+                    transforms.CenterCrop(256),
+                    transforms.Resize(img_size),
+                    transforms.ToTensor(),
+                    transforms.RandomAffine(
+                        degrees=(-20, 20), translate=(0.1, 0.1), scale=(0.9, 1.1)
+                    ),
+                ]
+            )
+
+            self.cvae_img_transform = transforms.Compose(
+                [
                     transforms.ToTensor(),
                     transforms.RandomAffine(
                         degrees=(-20, 20), translate=(0.1, 0.1), scale=(0.9, 1.1)
@@ -184,11 +216,23 @@ class DatasetCVAE(torch.utils.data.Dataset):
                 ]
             )
         # standardise intensities based on mean and std deviation
-        self.train_data_mean = np.mean(self.mr_image_list)
-        self.train_data_std = np.std(self.mr_image_list)
-        self.norm_transform = transforms.Normalize(
-            self.train_data_mean, self.train_data_std
+        self.pt_train_data_mean = np.mean(self.pt_mr_image_list)
+        self.pt_train_data_std = np.std(self.pt_mr_image_list)
+        self.pt_norm_transform = transforms.Normalize(
+            self.pt_train_data_mean, self.pt_train_data_std
         )
+
+        self.cvae_train_data_mean = np.mean(self.cvae_mr_image_list)
+        self.cvae_train_data_std = np.std(self.cvae_mr_image_list)
+        self.cvae_norm_transform = transforms.Normalize(
+            self.cvae_train_data_mean, self.cvae_train_data_std
+        )
+
+        self.mr_image_list = self.pt_mr_image_list + self.cvae_mr_image_list
+        self.mask_list = self.pt_mask_list + self.cvae_mask_list
+        # number of patients and slices in the dataset
+        self.no_patients = len(self.mr_image_list)
+        self.no_slices = self.mr_image_list[0].shape[0]
 
     def __len__(self):
         """Returns length of dataset"""
@@ -208,17 +252,23 @@ class DatasetCVAE(torch.utils.data.Dataset):
         the_slice = index - (patient * self.no_slices)
 
         seed = np.random.randint(2147483647)  # make a seed with numpy generator
-        random.seed(seed)
-        torch.manual_seed(seed)
 
-        x = self.norm_transform(
-            self.img_transform(self.mr_image_list[patient][the_slice, ...]).float()
-        )
         random.seed(seed)
         torch.manual_seed(seed)
-        y = self.img_transform(
-            (self.mask_list[patient][the_slice, ...] > 0).astype(np.int32)
-        )
+        x = self.mr_image_list[patient][the_slice, ...]
+        if x.shape[-1] == 64:
+            x = self.cvae_norm_transform(self.cvae_img_transform(x))
+        else:
+            x = self.pt_norm_transform(self.pt_img_transform(x).float())
+        
+        random.seed(seed)
+        torch.manual_seed(seed)
+        y = self.mask_list[patient][the_slice, ...] > 0
+        if y.shape[-1] == 64:
+            y = self.cvae_img_transform((y).astype(np.int32))
+        else:
+            y = self.pt_img_transform((y).astype(np.int32))
+
         return x, y
 
 
@@ -287,22 +337,10 @@ class CombinedProstateMRDataset(torch.utils.data.Dataset):
                 ]
             )
         
-        total_sum = 0
-        total_sq_sum = 0
-        total_count = 0
-        for arr in self.mr_image_list:
-            total_sum += arr.sum()
-            total_sq_sum += (arr**2).sum()  # Sum of squares
-            total_count += arr.size
-
-        mean_value = total_sum / total_count
-        std_value = np.sqrt((total_sq_sum / total_count) - (mean_value ** 2))
-        self.train_data_mean = mean_value
-        self.train_data_std = std_value
-
         # standardise intensities based on mean and std deviation
-        # self.train_data_mean = np.mean(self.mr_image_list)
-        # self.train_data_std = np.std(self.mr_image_list)
+        self.train_data_mean = np.mean(np.hstack([im.flatten() for im in self.mr_image_list if im.shape[-1] != 64]))
+        self.train_data_std = np.std(np.hstack([im.flatten() for im in self.mr_image_list if im.shape[-1] != 64]))
+        
         self.norm_transform = transforms.Normalize(
             self.train_data_mean, self.train_data_std
         )
@@ -329,7 +367,7 @@ class CombinedProstateMRDataset(torch.utils.data.Dataset):
         torch.manual_seed(seed)
         x = self.mr_image_list[patient][the_slice, ...]
         if x.shape[-1] == 64:
-            x = self.norm_transform(self.img_transform_cvae(self.mr_image_list[patient][the_slice, ...]).float())
+            x = self.img_transform_cvae(self.mr_image_list[patient][the_slice, ...]).float()
         else:
             x = self.norm_transform(self.img_transform(self.mr_image_list[patient][the_slice, ...]).float())
         
